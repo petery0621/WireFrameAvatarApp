ae5f27e200dc821924ae4495dba5a54c93f5d7ed
Jason Chiu <jason.chiu@vartyrcore.link>
2015-01-02 16:51:23 -0800

    Creating test repository for PB Project
    This is where we’ll show test scenes, content, and animations before
    pushing to live.

---
+++ Assets/doSomething.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doSomething : MonoBehaviour {
+
+	public void startGame()
+	{
+		
+		Debug.Log("Let's begin ");
+		Application.LoadLevel ("main");
+		
+	}
+
+	public void exit()
+	{
+		
+		Debug.Log("Let's leave ");
+		Application.Quit ();
+
+		}
+
+	public void back()
+
+	{
+		Debug.Log("Let's go back to home ");
+		Application.LoadLevel("home");
+
+		}
+
+
+	public void showCredits()
+	{
+		Debug.Log("Loading Credits Page ");
+		Application.LoadLevel ("credits");
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log("say something when i am made");
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	
+	}
+}
