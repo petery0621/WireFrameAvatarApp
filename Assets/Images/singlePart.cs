9512fb31f8f197cd38e3f78cfa7e056515647273
foreza <jforeza@gmail.com>
2015-01-03 13:07:03 -0800

    Scripts and other test assets

---
+++ Assets/Images/singlePart.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class singlePart : MonoBehaviour {
+
+	bool toggle; // 0 for not clicked, 1 for clicked. Simple.
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("This object: " + this.name + " status is: " + toggle);
+		toggle = false; // set to false on init.
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void printStatus()
+	{
+		Debug.Log ("This object: " + this.name + " status is: " + toggle);
+
+    }
+
+	public void onSelect() // when button is pressed, run this method. 
+	{
+		toggle = !toggle; // swaps the value.
+		Debug.Log ("This object: " + this.name + " was just pressed!");
+		// change color of object here.
+
+		if (toggle)
+			this.transform
+		else
+		this.renderer.material.color = Color.clear;
+		
+	}
+
+	// Accessor Method
+
+	public bool getStatus()
+	{
+		return toggle; 
+	}
+
+}
---
+++ Assets/Images/testBodyPart.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testBodyPart : MonoBehaviour {
+
+	public GameObject[] bodyParts;
+
+	// Use this for initialization
+	void Start () {
+
+		bodyParts = GameObject.FindGameObjectsWithTag("bodyPart"); // array of bodyParts searches for everything with the tag, "bodyPart"
+
+		Debug.Log ("Number of body parts registered: " + bodyParts.Length);
+
+		for(int i = 0; i < bodyParts.Length; i++)
+		{
+			Debug.Log(bodyParts[i].name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Debug.Log ("Number of body parts registered: " + bodyParts.Length);
+
+		// Debug.Log ("Message recieved from: "); // when a limb is toggled on and off, we want to know.
+	
+	}
+
+	void showCurrentState() // used to print out current state of the body.
+	{
+
+	}
+
+	public void onPress()
+	{
+		Debug.Log ("Message recieved from: " + this.name);
+}
+}
